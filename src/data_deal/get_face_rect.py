ab087abe9007a60aea5cf285edd69e2ee0770c36
ruiming <zhourui@streamax.com>
2021-08-21 16:30:56 +0800

    get face detection

--- src/data_deal/get_face_rect.py
+++ src/data_deal/get_face_rect.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+print(sys.platform)
 if sys.platform == "darwin":
     print('=>>>>load data from mac platform')
     sys.path.append("/Users/zhourui/workspace/pro/source/yolov5")
@@ -20,31 +20,26 @@ from pathlib import Path
 import cv2
 
 from common_utils.face_det_python import scrfd
+from common_utils.face_det_python import scrfd
 from common_utils.scn_python import scn
 from common_utils.mmcls_python import mmcls_fer
 from common_utils import common
 
 from multiprocessing import Process, Lock, Value
 
-dir_label_map = {0:'Angry', 1:'Happy', 2:'Neutral', 3:'Sad'}
-#dir_label_map = {'angry_heavy':0, 'angry_light':0, 'happy':1, 'neutral':2, 'sad_heavy':3, 'sad_light':3}
-#dir_label_map = {'angry_heavy': 0, 'happy': 1, 'neutral': 2, 'sad_heavy': 3}
-
 def parse_args():
     parser = argparse.ArgumentParser(description='Build file list')
     parser.add_argument(
         'src_folder', type=str, help='root directory for the frames')
     parser.add_argument(
-        'out_dir', type=str, help='out name')
+        'out_dir', type=str, help='out dir name')
+    parser.add_argument(
+        '--ext', type=str, default='jpg', help='out name')
     parser.add_argument(
         '--use_scrfd',
         action='store_true',
         default=False,
         help='choose face detection handler, yolov5 or scrfd')
-    parser.add_argument(
-        '--ext', type=str, default='jpg', help='out name')
-    parser.add_argument(
-        '--mmcls_src_dir', default='/Users/zhourui/workspace/pro/source/mmclassification', type=str, help='mmclassification source dir')
     parser.add_argument(
         '--level',
         type=int,
@@ -66,8 +61,8 @@ def parse_args():
     return args
 
 def frame_deal(paths, args, lock, counter, total_length):
-    # init face detection model
     if args.use_scrfd:
+        # init face detection model
         fd = scrfd.ScrdfFaceDet(0.45,
                                 model_path='common_utils/face_det_python/models/model.pth',
                                 device='cpu' if args.cpu else 'cuda',
@@ -76,20 +71,16 @@ def frame_deal(paths, args, lock, counter, total_length):
         from FaceDetection import FaceDetect
         args.weights = os.path.join(yolov5_src, 'weights/200_last.pt')
         args.imgsz = 640
-        fd = FaceDetect(args=args)
-
-    # init facial expression model : mmclassification
-    fer_mmcls = mmcls_fer.MMCLSFer(config_file_path='../common_utils/mmcls_python/models/mobilenet_v2/mobilenet_v2.py',
-                                   ckpt_path='../common_utils/mmcls_python/models/mobilenet_v2/latest.pth',
-                                   device='cpu' if args.cpu else 'cuda')
-    # init facial expression model : SCN
-    fer_scn = scn.ScnFacialExpressionCat(model_path='../common_utils/scn_python/models/epoch26_acc0.8615.pth', device='cpu' if args.cpu else 'cuda')
+        faceDetect = FaceDetect(args=args)
 
     for path in paths:
         frame = cv2.imread(path)
         if frame is None:
             continue
+
+        # face detection
         image = frame
+
         if args.use_scrfd:
             result = fd.forward(image)
             if len(result) < 1:
@@ -99,21 +90,15 @@ def frame_deal(paths, args, lock, counter, total_length):
             if prob < 0.45:
                 continue
         else:
-            bbox = fd.detect(image)
+            bbox = faceDetect.detect(image)
             # print(bbox)
             if len(bbox) != 4 or sum(bbox) < 1:
                 continue
             else:
                 sx, sy, ex, ey = bbox
 
-        # facial expression
-        #pred_label, pred_sclore, pred_name = fer_mmcls(image, [sx,sy,ex,ey])
-        pred_label, pred_sclore, pred_name = fer_scn(image, [sx, sy, ex, ey])
-
         # debug
         cv2.rectangle(image, (sx, sy), (ex, ey), (255, 0, 0), 10)
-        cv2.putText(image, '{}:{:.3f}'.format(pred_name, pred_sclore), (sx, sy - 20),
-                    0, 2, (0, 0, 255), 2)
         cv2.imshow('debug', image)
         if cv2.waitKey(0) & 0xff == ord('q'):
             break
@@ -166,4 +151,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
