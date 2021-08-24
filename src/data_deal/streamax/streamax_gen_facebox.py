f5e278e7b8ac7274b62c85d32875635966de5d69
zhourui <zhourui@streamax.com>
2021-08-24 16:00:06 +0800

    add code gen streamax facerect

--- src/data_deal/streamax/streamax_gen_facebox.py
+++ src/data_deal/streamax/streamax_gen_facebox.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append('..')
 import os
 import numpy as np
 import argparse
@@ -9,6 +10,8 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import xml.dom.minidom
 import cv2
+from utils import common
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Build file list')
@@ -31,8 +34,14 @@ def get_xml_info(xmlpath):
     dom = xml.dom.minidom.parse(xmlpath)
     root = dom.documentElement
 
-    nodes = root.getElementsByTagName('object')
+    # get h and w
+    size_node = root.getElementsByTagName('size')[0]
+    width_node = size_node.getElementsByTagName('width')[0]
+    width = int(float(width_node.childNodes[0].data))
+    height_node = size_node.getElementsByTagName('height')[0]
+    height = int(float(height_node.childNodes[0].data))
 
+    nodes = root.getElementsByTagName('object')
     ret = []
     for object_node in nodes:
         name_node = object_node.getElementsByTagName('name')[0]
@@ -56,29 +65,51 @@ def get_xml_info(xmlpath):
 
         ret.append([xmin,ymin,xmax,ymax])
 
+    return ret,height,width
+
+def get_new_rect(rects, h, w):
+    ret = []
+    for rect in rects:
+        xmin,ymin,xmax,ymax = rect
+        face_h = ymax - ymin
+        face_w = xmax - xmin
+
+        exp_h = int(face_h*0.2)
+        ymin = max(0, ymin-exp_h)
+        ret.append([xmin,ymin,xmax,ymax])
     return ret
 
-def deal(np_files):
-    for f in np_files:
-        # get xml file path
-        img_dir = os.path.basename(os.path.dirname(f))
-        xml_dir = 'xml'+img_dir[3:]
-        xml_name = os.path.basename(f)[:-3]+'xml'
-        xml_path = os.path.join(os.path.dirname(os.path.dirname(f)), xml_dir, xml_name)
-        if not os.path.exists(xml_path):
-            print('Can not find xml : ', xml_path)
-            continue
 
-        # get xml info
-        face_rects = get_xml_info(xml_path)
+def deal(f):
+    # get xml file path
+    img_dir = os.path.basename(os.path.dirname(f))
+    xml_dir = 'xml'+img_dir[3:]
+    xml_name = os.path.basename(f)[:-3]+'xml'
+    xml_path = os.path.join(os.path.dirname(os.path.dirname(f)), xml_dir, xml_name)
+    if not os.path.exists(xml_path):
+        print('Can not find xml : ', xml_path)
+        return True
 
-        # debug
-        for rect in face_rects:
-            xmin,ymin,xmax,ymax = rect
-            image = cv2.imread(f)
-            image = cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (0,0,255), 2)
-            cv2.imshow('1', image)
-            cv2.waitKey(0)
+    # get xml info
+    face_rects,height,width = get_xml_info(xml_path)
+    if len(face_rects) != 1:
+        # check if have no face, or multi face
+        return True
+    new_face_rects = get_new_rect(face_rects, height, width)
+
+    # save info
+    txt_save_path = os.path.join(os.path.dirname(f), os.path.basename(f)[:-3]+'facerect')
+    common.save_lists_to_txtfile(new_face_rects, txt_save_path)
+    # debug
+    # for rect in face_rects:
+    #
+    #     image = cv2.imread(f)
+    #     image = cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (0,0,255), 2)
+    #     cv2.imshow('1', image)
+    #     key = cv2.waitKey(0)
+    #     if key==ord('q'):
+    #         return True
+    return True
 
 def main():
     args = parse_args()
